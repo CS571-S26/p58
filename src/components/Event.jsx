f92f873b2b1a08b1621bacba0cb0bde16768e495
sidney <sheberlein@wisc.edu>
2026-04-25 10:35:27 -0500

    fixing accessibility

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -4,8 +4,8 @@ export default function Event(props) {
   return (
     <Card>
       <Card.Body>
-        <Card.Title as="h3">{props.title}</Card.Title>
-        <Card.Subtitle as="h4" className="mb-2 text-muted">{props.date}</Card.Subtitle>
+        <Card.Title as="h2">{props.title}</Card.Title>
+        <Card.Subtitle as="h3" className="mb-2 text-muted">{props.date}</Card.Subtitle>
         <Card.Text>{props.description}</Card.Text>
       </Card.Body>
     </Card>
